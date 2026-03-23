b997aa4660dae6d60158210d1cd750752011ac06
Zhibek <zhibekaismaberg@gmail.com>
2026-03-23 13:57:12 +0600

    changed js

--- script.js
+++ script.js
@@ -60,21 +60,84 @@ console.log(arr2) */   // result: [1,2,3,4]  [1,2,3,4]
 /* let numbers = [1, 2, 3, 4, 5]    
 let numbers1 = [6, 7, 8, 9]  
 console.log(numbers.concat(numbers1)); */   // result: [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+/* let numbers = [1, 2, 3, 4, 5]
+let numbers1 = [6, 7, 8, 9, 10] */
+/* let numbers2 = numbers.slice()
+numbers.push(6)
+console.log(numbers); */      // 1 2 3 4 5 6
+/* console.log(numbers.join());    // 1,2,3,4,5 str
+console.log(numbers.includes(4));  // true
+console.log(numbers.indexOf(5)); */   // 4
+
+
+// Iteration methods
                           
 /* const favorites = [
-    {name: "Ronaldo", age: 42}
-    {name: "Joe Biden", age: 39}
-    {name: "Adele", age: 40}
-    {name: "Barak Obama", age: 42}
-    {name: "Beyonce", age: 44}
-    {name: "Justin Bieber", age: 31}
-    {name: "Bil Gates", age: 69}
-    {name: "Jeff Bezos", age: 61}
-    {name: "Mark Zuckerberg", age: 41}
+    {name: "Ronaldo", age: 42},
+    {name: "Joe Biden", age: 39},
+    {name: "Adele", age: 40},
+    {name: "Barak Obama", age: 42},
+    {name: "Beyonce", age: 44},
+    {name: "Justin Bieber", age: 31},
+    {name: "Bil Gates", age: 69},
+    {name: "Jeff Bezos", age: 61},
+    {name: "Mark Zuckerberg", age: 41},
 ];
+
+favorites.forEach(() => {
+  console.log("called");
+})
+
 favorites.forEach((item, index) => {
-    console.log(index);
-})         */                      
+    console.log(item, index);
+})         
+console.log(
+    favorites.map((item) => {
+        return item.name + "is" + item.age + "years old"
+    })
+);
+console.log(
+  favorites.filter((item) => {
+    return item.age > 40
+  })
+)
+console.log(
+    favorites.some((item) => {
+        return item.age === 60
+    })
+);
+console.log(
+    favorites.every((item) => {
+        return item.age > 40
+    })
+);               
+console.log(
+  favorites.find((item) => {
+    return item.age === 40
+  })
+);
+console.log(
+  favorites.findIndex((item) => {
+    return item.age === 40
+  })
+);
+
+console.log(
+  favorites.sort((a, b) => {
+    if (a.age < b.age) return 1;
+    else return -1
+  })
+);
+
+
+const number = [6, 2, 4, 18, 92, 23]
+number.sort((a, b) => {
+  if (a > b) return 1;
+  else return -1;
+})
+console.log(number);
+ */
 
 
 
@@ -95,11 +158,11 @@ const numbers3 = numbers.map((number) => number * 5);
 console.log(numbers3);
 
 numbers.sort((a, b) => a - b);
-console.log(numbers); */
-
+console.log(numbers); 
+ */
 // 2
 
-const famouses = [
+ const famouses = [
   { name: "Cristiano Ronaldo", age: 41, role: "footballer" },
   { name: "Joe Biden", age: 83, role: "politician" },
   { name: "Adele", age: 37, role: "singer" },
@@ -112,16 +175,79 @@ const famouses = [
   { name: "Billie Eilish", age: 24, role: "singer" }
 ];
 
-const names = famouses.map((famouse) => famouse.name);
+console.log(
+  famouses.map((item) => {     // 1 вариант
+    return item.name
+  })
+);
+
+const names = famouses.map((famouse) => famouse.name); // 2 вариант
 console.log(names);
 
+console.log(
+  famouses.map((item) => {
+    return item.name + " is a " + item.role // 1 вариант
+  })
+);
+
 const descriptions = famouses.map(
-  (famouse) => `${famouse.name} is a ${famouse.role}`
+  (famouse) => `${famouse.name} is a ${famouse.role}`  // 2 вариант
 );
 console.log(descriptions);
+
+console.log(
+  famouses.reduce((prev, curr) => {     
+    return prev + curr.age;
+  }, 0) / famouses.length          // средний возраст
+);
+
+console.log(
+  famouses.reduce((prev, curr) => {
+    if (prev.age > curr.age) return prev;   // самый старший
+    else return curr;
+  })
+);
+
+console.log(
+  famouses.reduce((prev, curr) => {
+    if (prev.age < curr.age) return prev;   // самый младший
+    else return curr;
+  })
+);
+
+console.log(
+  famouses.reduce((prev, curr) => {
+    if (prev.age < curr.age) return prev;  // самый старший 
+    else return curr
+  }).age - 
+  famouses.reduce((prev, curr) => {
+    if (prev.age > curr.age) return prev;  // самый младший
+    else return curr
+  }).age
+);
+
+console.log(
+  famouses.sort((a, b) => {
+    if (a.age > b.age) return 1
+    else return -1
+  })
+);
+
+console.log(
+  sortedFamouses[0],
+  sortedFamouses[sortedFamouses.length -1],
+);
+
+
+console.log(
+  famouses.reduce((prev, curr) => {
+    return prev + curr.age;
+  },0),
+);
 
 const unage = famouses.some((famouse) => famouse.age < 18);
 console.log(unage);
 
 const olderThan20 = famouses.filter((famouse) => famouse.age > 20);
-console.log(olderThan20);
+console.log(olderThan20); 
+
